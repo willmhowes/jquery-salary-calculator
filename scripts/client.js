81a1222a921aba0046b9ba07bcb33fac70b82741
Will Howes <willmhowes@gmail.com>
2019-03-09 19:09:03 -0600

    begin wireframe outlining

--- scripts/client.js
+++ scripts/client.js
@@ -1,5 +1,25 @@
 $(document).ready( onReady);
 
+class Employee {
+  constructor(firstName, lastName, idNumber, jobTitle, annualSalary) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.idNumber = idNumber;
+    this.jobTitle = jobTitle;
+    this.annualSalary = annualSalary;
+  }
+
+  addToDOM() {
+
+  }
+}
+
+
 function onReady() {
   console.log('jq');
+  $('#submitForm').on('click', createNewObject)
+}
+
+function createNewObject() {
+
 }
