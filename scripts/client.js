323072ca195ea6fe90b3fc31acf4eb4fa2d02532
Will Howes <willmhowes@gmail.com>
2019-03-09 20:05:29 -0600

    add 'show footer'

--- scripts/client.js
+++ scripts/client.js
@@ -1,21 +1,20 @@
 $(document).ready( onReady);
 
 let employeeList = [];
+let showFooter = false;
 
 class Employee {
   constructor(firstName, lastName, idNumber, jobTitle, annualSalary) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.idNumber = idNumber;
-    this.jobTitle = jobTitle;
-    this.annualSalary = annualSalary;
-    // this.caclulation = methodForCalculation();
+	 this.firstName = firstName;
+	 this.lastName = lastName;
+	 this.idNumber = Number(idNumber);
+	 this.jobTitle = jobTitle;
+	 this.annualSalary = Number(annualSalary);
   }
 
   // Methods go here
 
 }
-
 
 function onReady() {
   console.log('jq');
@@ -26,11 +25,11 @@ function createNewObject() {
   console.log('button works');
 
   let newEmployee = new Employee (
-    $('#firstName').val(),
-    $('#lastName').val(),
-    $('#idNumber').val(),
-    $('#jobTitle').val(),
-    $('#annualSalary').val(),
+	 $('#firstName').val(),
+	 $('#lastName').val(),
+	 $('#idNumber').val(),
+	 $('#jobTitle').val(),
+	 $('#annualSalary').val(),
   )
 
   employeeList.push(newEmployee);
@@ -44,22 +43,36 @@ function handleSubmit(event) {
 
 function render() {
   $('#tableBody').empty();
+  $('#monthlyCost').empty();
+
+  // Shows the footer after 5 employees have been added to the array
+  if (showFooter == false && employeeList.length > 4) {
+	 $('#footer').append(`<tr>
+	 <th>First Name</th>
+	 <th>Last Name</th>
+	 <th>ID Num</th>
+	 <th>Job Title</th>
+	 <th>Annual Salary</th>
+	 </tr>`)
+
+	 showFooter = true;
+  }
 
   let monthlyCost = 0;
   let $employeeData;
 
   for (let employee of employeeList) {
-    $employeeData = $(
-    `<tr><td>${employee.firstName}</td>
-    <td>${employee.lastName}</td>
-    <td>${employee.idNumber}</td>
-    <td>${employee.jobTitle}</td>
-    <td>${employee.annualSalary}</td></tr>`
-    );
+	 $employeeData = $(`<tr>
+	 <td>${employee.firstName}</td>
+	 <td>${employee.lastName}</td>
+	 <td>${employee.idNumber}</td>
+	 <td>${employee.jobTitle}</td>
+	 <td>${employee.annualSalary}</td>
+	 </tr>`);
 
-    monthlyCost += employee.annualSalary / 12;
+	 monthlyCost += employee.annualSalary / 12;
 
-    $('#tableBody').append($employeeData);
+	 $('#tableBody').append($employeeData);
   }
 
   $('#monthlyCost').append(`Monthly Cost: $${Math.round(monthlyCost * 100) / 100}`);
